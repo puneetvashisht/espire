6db30962744625e9220362a4dba430ea569232ff
Aditya Singh <aditya.singh@espire.com>
2022-10-31 17:10:23 +0530

    using maps and list

--- react/my-first-app/src/App.js
+++ react/my-first-app/src/App.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import './App.css';
 import Badge from './Badge';
 import Card from './Card';
@@ -6,14 +7,25 @@ import Clock from './Clock';
 import Message from './Message';
 
 function App() {
+  const [courses, setCourses] = useState([
+    {id:1, title: 'React'},
+    {id:3, title: 'Angular'},
+    {id:2, title: 'NodeJS'},
+    {id:4, title: '.NET'},
+  ])
+
+  let courseList = courses.map((course, i)=> <Card key={course.id} title={course.title}/>);
+  console.log(courseList);
+
   return (
     <div className="container row">
-      <Card title="React JS"/>
+      {courseList}
+      {/* <Card title="React JS"/>
       <Card title="Angular JS"/>
       <Card title="React JS"/>
       <Card title="Angular JS"/>
       <Card title="React JS"/>
-      <Card title="Angular JS"/>
+      <Card title="Angular JS"/> */}
       {/* <Clock now={new Date()}></Clock>
       <Badge caption="Inbox"></Badge>
       <Badge caption="Sent"></Badge>
