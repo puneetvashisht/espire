d5260ba93075c750fb773283d55311c91b7c5425
Aditya Singh <aditya.singh@espire.com>
2022-11-01 15:19:28 +0530

    delete & add

--- react/my-second-app/src/App.js
+++ react/my-second-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ViewEmployees from './components/ViewEmployees';
+import AddEmployee from './components/AddEmployee';
 
 function App() {
   return (
     <div className="container row">
+      <AddEmployee></AddEmployee>
+      <hr/>
       <ViewEmployees></ViewEmployees>
     </div>
   );
--- react/my-second-app/src/components/ViewEmployees.js
+++ react/my-second-app/src/components/ViewEmployees.js
@@ -12,13 +12,30 @@ export default function ViewEmployees() {
     })
   }, [])
 
+  const deleteEmployee = (id) => {
+
+    let fileteredEmployees = employees.filter((employee)=>employee.id != id)
+
+    fetch('http://localhost:8000/employees/' + id, {
+        method: "DELETE"
+    })
+    .then(res=>{
+        console.log(res);
+        return res.json();
+    })
+    .then((data)=> {
+        console.log(data)
+        setEmployees(fileteredEmployees)
+    })
+  }
+
   let employeeList = employees.map((employee)=> {
     return (
         <tr key={employee.id}>
       <th scope="row">{employee.id}</th>
       <td>{employee.name}</td>
       <td>{employee.salary}</td>
-      <td>@mdo</td>
+      <td><button onClick={()=>deleteEmployee(employee.id)} className='btn btn-danger'> X </button></td>
     </tr>
     )
   })
